afadb1dd5d9959d6d3a93b0fbf11a3d76459e4a0
EC2 Default User <ec2-user@ip-172-31-91-94.ec2.internal>
2025-04-26 03:56:04 +0000

    Cambios para despliegue correcto

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'helloworld_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangoproject',
+        'NAME': 'djangodocker',
         'USER': 'admin',
-        'PASSWORD': '5AmrrWDc3JNX9Vx',
-        'HOST': 'teis20251.cpsc8aigcnqs.us-east-1.rds.amazonaws.com',
+        'PASSWORD': 'WrJYSmFBH9u7Qui',
+        'HOST': 'teis20251.c7wsgi428q2i.us-east-1.rds.amazonaws.com',
         'PORT': '3306'
     }
 }
